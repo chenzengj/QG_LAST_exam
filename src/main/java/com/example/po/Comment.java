f21cc1d89130f92fabd89750827a525d03aada58
chenzengj <142132169+chenzengj@users.noreply.github.com>
2024-04-27 03:07:26 +0800

    Add files via upload

---
+++ src/main/java/com/example/po/Comment.java
@@ -0,0 +1,22 @@
+package com.example.po;
+
+public class Comment {
+    private int poseId;
+    private String content;
+
+    public int getPoseId() {
+        return poseId;
+    }
+
+    public void setPoseId(int poseId) {
+        this.poseId = poseId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
